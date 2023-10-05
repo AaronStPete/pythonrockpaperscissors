ff86ba071dcf14ec05f3a71dcd8ec31b91024d97
Aaron Wasserman <36515361+AaronStPete@users.noreply.github.com>
2023-10-05 10:15:20 -0400

    first working iteration
    made this file using mu 1.2.0

---
+++ rockpaperscissors.py
@@ -0,0 +1,59 @@
+import sys, random
+
+while True:
+
+    print('Please enter "rock" "paper" or "scissors" or the first letter.')
+    guess = input()
+    guessNum = 0
+    if guess == 'rock' or guess == 'r':
+        guessNum = 1
+    elif guess == 'paper' or guess == 'p':
+        guessNum = 2
+    elif guess == 'scissors' or guess == 's':
+        guessNum = 3
+    elif guess == 'exit':
+            sys.exit()
+    else:
+        print('Your input is invalid.')
+        break
+
+
+    compGuessNum = random.randint(1,3)
+    compGuess = ''
+    if compGuessNum == 1:
+        compGuess = 'rock'
+    elif compGuessNum == 2:
+        compGuess = 'paper'
+    elif compGuessNum == 3:
+        compGuess = 'scissors'
+    print('The computer threw down ' + compGuess)
+
+
+    if compGuessNum == guessNum:
+        print('No winner.')
+    elif (compGuessNum == 1 and guessNum == 2) or (compGuessNum == 2 and guessNum == 3) or (compGuessNum == 3 and guessNum == 1):
+        print('You win!')
+    else:
+        print ('You lose.')
+
+
+    print('Try again')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
